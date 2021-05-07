a157ba42e76bbf916b0fd81048535a9655211201
a0979470582 <a0979470582@gmail.com>
2021-05-07 21:15:53 +0800

    進度: 正在製作MusicPlayer

---
+++ app/src/main/java/com/bu/selfstudy/tool/MusicPlayer.kt
@@ -0,0 +1,9 @@
+package com.bu.selfstudy.tool
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
+class MusicPlayer(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
+
+}
